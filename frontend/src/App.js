4527c1d5aaa8db5f2104838d15dde1cc990f68f0
Maharani Eka Pratiwi <raniangelo.r@gmail.com>
2019-11-13 23:51:42 +0700

    KELAR DONG YA TUHAN

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import List from "./components/List";
 import dummyItems from "./items.json";
+import EmptyState from "./components/EmptyState";
 
 export default class App extends React.Component{
-  state={
-    favItems:[]
+  constructor(props){
+    super(props)
+    this.state = { 
+      favItems : [], 
+      show : true 
+    };
+    this.handleToggle = this.handleToggle.bind(this)
   }
 
   handleItemClick = item => {
@@ -14,29 +20,49 @@ export default class App extends React.Component{
     if(targetInd<0) newItems.push(newItem);
     this.setState({favItems: newItems});
   }
-  render(){
-    const { favItems } = this.state;
-    return(
+
+  handleRemoveFavorite = item=> {
+    const newItems = [...this.state.favItems];
+    const newItem = {...item};
+    const targetInd = newItems.findIndex(it=>it.id===newItem.id);
+    if(targetInd<0) newItems.push(newItem);
+    else newItems.splice(targetInd, 1);
+    this.setState({favItems: newItems});
+  };
+
+  handleToggle = () => {
+    const {show} = this.state;
+    this.setState({show : !show});
+  }
+
+   render() { 
+    const {favItems} = this.state;
+    return (
       <div className="container-fluid">
-        <h1 className="text-center">
-          Welcome!
-          <small>Class-based</small>
-        </h1>
+        <h1 className="text-center">Welcome! <small>Class-based</small></h1>
         <div className="container pt-3">
+        <label className="container float-right">
+           <input type="checkbox" onChange={this.handleToggle}/> Show Favorite
+           <span className="checkmark"></span>
+        </label>
           <div className="row">
-            <div className="col-sm">
-              <List 
-                title="Our Menu" 
-                items={dummyItems} 
-                onItemClick={this.handleItemClick}/>
-            </div>
-            <div className="col-sm">
-              <List 
-                title="My Favorite"
-                items={favItems}
-                onItemClick={this.handleItemClick}
+          <div className="col-sm">
+              <List
+              title = "Our Menu"
+              items = {dummyItems}
+              onItemClick = {this.handleItemClick}
               />
             </div>
+            {!this.state.show ?
+              <div className="col-sm">
+                {!this.state.favItems.length ? <EmptyState/> :
+                <List
+                title = "My Favorite"
+                items = {favItems} 
+                onItemClick = {this.handleRemoveFavorite}
+                />}
+              </div> 
+            : null}
           </div>
         </div>
       </div>
